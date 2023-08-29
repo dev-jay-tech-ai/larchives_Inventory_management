1b12b8982a936e5100bdbd478ff37552d66d4785
jaewonks <jaewonaaart@gmail.com>
2023-08-29 17:01:50 +0100

    290823 deploy_test1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { config as dotenvConfig } from 'dotenv';
+import { config as dotenvConfig } from 'dotenv'
 
 // Load root .env file
 dotenvConfig();
