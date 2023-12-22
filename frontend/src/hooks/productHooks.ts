0a4b9028f365851bf9c04e18cd058ffd064aebcd
jaewonks <jaewonaaart@gmail.com>
2023-12-22 16:24:35 +0000

    1624 221223 updates

--- frontend/src/hooks/productHooks.ts
+++ frontend/src/hooks/productHooks.ts
@@ -43,7 +43,7 @@ export const useDeleteProductsMutation = () =>
 export const useShopifyInventoryMutation = () => 
   useMutation(
     async () => {
-      await apiClient.get('/api/db/insert/shopify/inventory');
+      await apiClient.get('/api/db/insert');
     },
     {
       onError: (error) => {
